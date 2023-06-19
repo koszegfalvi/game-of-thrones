1ab89feeee4435a5252afbc46a14fc1914ad44a7
Admin <koszegfalvi.gabor@gmail.com>
2023-06-19 14:31:49 +0200

    continue

---
+++ src/MeleeWeapon.java
@@ -0,0 +1,10 @@
+
+public class MeleeWeapon extends Weapon {
+
+    private int ammunition;
+    public MeleeWeapon(String name, int damage, int range,) {
+        super(name, damage, range=1);
+
+    }
+}
+
---
+++ src/Weapon.java
@@ -0,0 +1,18 @@
+
+public abstract class Weapon {
+    private String name;
+    private int damage;
+
+    public Weapon(String name, int damage, int range) {
+        this.name = name;
+        this.damage = damage;
+        this.range = range;
+    }
+
+    private int  range;
+    public void attack(){
+
+    }
+
+
+}
